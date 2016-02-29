85dc40fe7c55fb86a570e637c3016f1969ea98d8
ataroshe <ataroshe@gmail.com>
2016-02-29 23:24:49 +0200

    update

--- src/geometry/Trangle.java
+++
@@ -1,9 +0,0 @@
-package geometry;
-
-public class Trangle {
-
-    public static int trianglePerimeter(int triangleX, int triangleY, int triangleZ){
-        int trianglePerimeter = triangleX + triangleY + triangleZ;
-        return trianglePerimeter;
-    }
-}
