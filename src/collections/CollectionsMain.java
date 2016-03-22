dab9dfe699f2c43b35b52cc4c97b3853432bf807
User <ataroshe@gmail.com>
2016-03-22 21:05:47 +0200

    Lesson 22.03 TDD, LinkedList, Set

--- src/collections/CollectionsMain.java
+++ src/collections/CollectionsMain.java
@@ -1,8 +1,9 @@
 package collections;
 
 import Figure.Cat;
+import com.sun.org.apache.xpath.internal.SourceTree;
 
-import java.util.ArrayList;
+import java.util.*;
 
 /**
  * Created by User on 18.03.2016.
@@ -17,6 +18,40 @@ public class CollectionsMain {
 
         System.out.println(list);
 
-        Object tom = list.get(1);
+        Cat tom = list.get(1);
+
+        LinkedList<Cat> linkedList = new LinkedList<>();
+        linkedList.add(new Cat("tom", "grey", 2015));
+        linkedList.add(new Cat("tom", "grey", 2015));
+        linkedList.add(new Cat("tom", "grey", 2015));
+        linkedList.add(new Cat("tom", "grey", 2015));
+
+        System.out.println(linkedList.get(1));
+
+        printList(list);
+        printList(linkedList);
+
+        Collection<Cat> cats = linkedList;
+
+        TreeSet<Cat> sortedCats = new TreeSet<>(new CatComparator());
+        sortedCats.add(new Cat("tom", "grey", 2015));
+        sortedCats.add(new Cat("tom", "grey", 2011));
+        sortedCats.add(new Cat("tom", "grey", 2013));
+        sortedCats.add(new Cat("Murzik", "grey", 2013));
+
+        printForEach(sortedCats);
+    }
+
+    public static void printForEach(Collection<Cat> collection){
+        for (Cat cat : collection) {
+            System.out.println(cat);
+        }
+    }
+
+
+    public static void printList (List<Cat> cats) {
+        for (int i = 0; i < cats.size(); i++) {
+            System.out.println(cats.get(i));
+        }
     }
 }
---
+++ src/collections/CustomLinkedList.java
@@ -0,0 +1,37 @@
+package collections;
+
+/**
+ * Created by User on 22.03.2016.
+ */
+public class CustomLinkedList {
+
+    private ListElement next;
+
+    public int size(){
+        if (next == null) {
+            return 0;
+        }
+        else {
+            return next.size();
+        }
+    }
+
+    public void add(Object o) {
+        if (next == null) {
+            next = new ListElement(o);
+        }
+        else {
+            next.add(o);
+        }
+
+    }
+
+    public Object get(int index){
+        if (next == null) {
+            return null;
+        }
+        else {
+            return next.get(index);
+        }
+    }
+}
